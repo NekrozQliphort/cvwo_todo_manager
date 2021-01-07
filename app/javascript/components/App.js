83b254c6754a7336f3054248dad73dc654560c0d
Ryan Chung <timelord0830@gmail.com>
2021-01-08 00:02:43 +0800

    Basic Page and Configs

---
+++ app/javascript/components/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+export default App
--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
-module.exports = function(api) {
-  var validEnv = ['development', 'test', 'production']
-  var currentEnv = api.env()
-  var isDevelopmentEnv = api.env('development')
-  var isProductionEnv = api.env('production')
-  var isTestEnv = api.env('test')
+module.exports = function (api) {
+  const validEnv = ['development', 'test', 'production']
+  const currentEnv = api.env()
+  const isDevelopmentEnv = api.env('development')
+  const isProductionEnv = api.env('production')
+  const isTestEnv = api.env('test')
 
   if (!validEnv.includes(currentEnv)) {
     throw new Error(
@@ -23,7 +23,8 @@ module.exports = function(api) {
           targets: {
             node: 'current'
           }
-        }
+        },
+        '@babel/preset-react'
       ],
       (isProductionEnv || isDevelopmentEnv) && [
         '@babel/preset-env',
@@ -33,6 +34,13 @@ module.exports = function(api) {
           corejs: 3,
           modules: false,
           exclude: ['transform-typeof-symbol']
+        }
+      ],
+      [
+        '@babel/preset-react',
+        {
+          development: isDevelopmentEnv || isTestEnv,
+          useBuiltIns: true
         }
       ]
     ].filter(Boolean),
