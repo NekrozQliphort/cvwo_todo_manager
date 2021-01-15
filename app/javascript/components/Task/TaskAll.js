fd86db040d39d740dd6e81d96dbf1a0dde2ff5a7
Ryan Chung <timelord0830@gmail.com>
2021-01-16 02:55:20 +0800

    Front End Update

--- app/javascript/components/Task/TaskAll.js
+++ app/javascript/components/Task/TaskAll.js
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ax from 'packs/ax'
 import _ from 'lodash'
-import { Box } from '@material-ui/core'
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650
+  }
+})
 
 const TaskAll = () => {
+  const [tasks, setTasks] = useState([])
+  const classes = useStyles()
+
+  function updateTasks () {
+    ax.get('/api/tasks')
+      .then(resp => setTasks(resp.data))
+      .catch(resp => console.log(resp))
+  }
+
+  useEffect(updateTasks, [])
+
+  const taskRows = _.map(tasks, (task, index) => {
+    const deadlineDate = new Date(task.deadline)
+    return (
+      <TableRow key={index}>
+        <TableCell>{task.title}</TableCell>
+        <TableCell>{`${deadlineDate.getDate()}/${deadlineDate.getMonth() + 1}/${deadlineDate.getFullYear()}`}</TableCell>
+        <TableCell></TableCell>
+      </TableRow>
+    )
+  })
+
   return (
     <Box>
       <h1>All The Tasks should be here</h1>
+      <TableContainer component={Paper} elevation={3}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell><strong>Task</strong></TableCell>
+              <TableCell><strong>Deadline</strong></TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {taskRows}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   )
 }
